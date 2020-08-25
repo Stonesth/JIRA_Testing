b6cc37a1ed9c361ada651febbc65d346f8fc41d1
Thonon <JF30LB@insim.biz>
2020-08-25 15:19:54 +0200

    First commit

---
+++ jira_testing.py
@@ -0,0 +1,30 @@
+from Tools import tools_v000 as tools
+from Topdesk import topdesk as t
+from Jira import jira as j
+import os
+from os.path import dirname
+
+
+# -12 for the name of this project JIRA_Testing
+save_path = dirname(__file__)[ : -12]
+propertiesFolder_path = save_path + "Properties"
+
+t.incidentNumber = "I2008-00972"
+j.sprint = "PNN-TOS-PI2020.3.5"
+
+j.epic_link = tools.readProperty(propertiesFolder_path, 'Incident_Lifenet', 'epic_link=')
+j.save_path = tools.readProperty(propertiesFolder_path, 'Incident_Lifenet', 'save_path=')
+
+def connectToJIRA() :
+    tools.driver.get('https://jira-test.atlassian.insim.biz/')
+
+
+# Open Browser
+tools.openBrowserChrome()
+
+# TopDesk part
+t.connectViaLink()
+t.incidentTitle()
+
+# Jira part
+j.createJira(jiraTitle, t.description_text, t.incidentNumber)
